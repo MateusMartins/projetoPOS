b8e47d71c15122b014e4ced1f7784e85de4f540c
mateus <mateusmferreira@live.com>
2018-06-23 22:29:50 -0300

    alterações de formulários e adição de comentários

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ import datetime
 
 app = Flask(__name__)
 
-# Config MySQL
+# Configuração do MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'myflaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-#config mongodb
+#configuração do mongoDB
 client = MongoClient()
 db = client['myflaskapp']
 collection = db['articles']
 
-# init MYSQL
+# inicialização do MYSQL
 mysql = MySQL(app)
 
 # Index
@@ -30,49 +30,45 @@ mysql = MySQL(app)
 def index():
     return render_template('home.html')
 
-# About
+# Sobre
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# Articles
+# Artigos
 @app.route('/articles')
 def articles():
-    # Create cursor
-    cur = mysql.connection.cursor()
-
-    # Get articles
+    # Listagem de todos os artigos no mongoDB
     result = collection.find()
+    # Adiciona os artigos em uma lista
     all_data = list(result)
 
+    # Valida se existe algum artigo cadastrado
     if all_data:
         return render_template('articles.html', articles=all_data)
     else:
-        msg = 'No Articles Found'
+        msg = 'Nenhum artigo cadastrado'
         return render_template('articles.html', msg=msg)
-    # Close connection
-    cur.close()
 
-#Single Article
+#Exibição de artigo
 @app.route('/article/<string:id>/')
 def article(id):
-    # Get article
+    # Seleciona o artigo que foi clicado pelo ObjectId(id)
     result = collection.find_one({'_id':ObjectId(id)})
-
     return render_template('article.html', article=result)
 
-# Register Form Class
+# Regras de validação do formulário de cadastro
 class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
+    name = StringField('Nome', [validators.Length(min=1, max=50)])
+    username = StringField('Usuário', [validators.Length(min=4, max=25)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
+    password = PasswordField('Senha', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
+        validators.EqualTo('confirm', message='Senhas diferentes')
     ])
-    confirm = PasswordField('Confirm Password')
+    confirm = PasswordField('Confirme sua senha')
 
-# User Register
+# Cadastro de usuário
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
@@ -80,135 +76,139 @@ def register():
         name = form.name.data
         email = form.email.data
         username = form.username.data
+        # Encriptografa a senha
         password = sha256_crypt.encrypt(str(form.password.data))
 
-        # Create cursor
+        # Criação do cursor
         cur = mysql.connection.cursor()
 
-        # Execute query
+        # Inserção do usuário no MySQL
         cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
 
-        # Commit to DB
+        # Commit no MySQL
         mysql.connection.commit()
 
-        # Close connection
+        # Encerra a conexão
         cur.close()
 
-        flash('You are now registered and can log in', 'success')
+        flash('Registrado com sucesso', 'success')
 
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
-# User login
+# Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Get Form Fields
+        # Pega o valor dos campos
         username = request.form['username']
         password_candidate = request.form['password']
 
-        # Create cursor
+        # Criação do cursor
         cur = mysql.connection.cursor()
 
-        # Get user by username
+        # Procura o usuário no MySQL
         result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
 
+        #Valida se encontrou o usuario no MySQL
         if result > 0:
-            # Get stored hash
             data = cur.fetchone()
+            #Pega a senha do usuário
             password = data['password']
 
-            # Compare Passwords
+            # Compara as senhas
             if sha256_crypt.verify(password_candidate, password):
-                # Passed
+                # Cria a sessão para o usuário
                 session['logged_in'] = True
                 session['username'] = username
 
-                flash('You are now logged in', 'success')
+                flash('Login efetuado com sucesso!', 'success')
                 return redirect(url_for('dashboard'))
             else:
-                error = 'Invalid login'
+                error = 'Usuário ou senha incorretos'
                 return render_template('login.html', error=error)
-            # Close connection
+            # Encerra a conexão
             cur.close()
         else:
-            error = 'Username not found'
+            error = 'Usuário ou senha incorretos'
             return render_template('login.html', error=error)
 
     return render_template('login.html')
 
-# Check if user logged in
+# Valida se o usuário está logado
 def is_logged_in(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
-            flash('Unauthorized, Please login', 'danger')
+            flash('Não autorizado, efetue login para acessar está página', 'danger')
             return redirect(url_for('login'))
     return wrap
 
-# Logout
+# Sair
 @app.route('/logout')
 @is_logged_in
 def logout():
     session.clear()
-    flash('You are now logged out', 'success')
+    flash('Sessão encerrada com sucesso! Volte sempre!', 'success')
     return redirect(url_for('login'))
 
 # Dashboard
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-    # Get articles
+    # Seleciona todos os artigos no mongoDB
     result = collection.find()
+    # Adiciona os objetos encontrados em uma lista
     all_data = list(result)
 
+    # Valida se alista não está vazia
     if all_data:
         return render_template('dashboard.html', articles=all_data)
     else:
-        msg = 'No Articles Found'
+        msg = 'Não existem artigos cadastrados'
         return render_template('dashboard.html', msg=msg)
 
-# Controle de caracteres
+# Controle do formulário do cadastro de artigo
 class ArticleForm(Form):
-    title = StringField('Title', [validators.Length(min=1, max=200)])
-    body = TextAreaField('Body', [validators.Length(min=30)])
+    title = StringField('Título', [validators.Length(min=1, max=200)])
+    body = TextAreaField('Corpo do texto', [validators.Length(min=30)])
 
-# Add Article
+# Adicionar artigo
 @app.route('/add_article', methods=['GET', 'POST'])
 @is_logged_in
 def add_article():
     form = ArticleForm(request.form)
     if request.method == 'POST' and form.validate():
         
-        #Carrega as informações da tela
+        # Carrega as informações da tela
         title = form.title.data
         body = form.body.data
 
-        #Mongo insert
+        # Cria as informações que serão inseridas no mongoDB
         post = {'title': title,'body': body , 'author': session['username'], 'create_date' : datetime.datetime.utcnow()}
 
-        #Adicionar documento
+        # Faz a inserção no mongoDB
         collection.insert_one(post)
 
-        flash('Article Created', 'success')
+        flash('Artigo criado com sucesso', 'success')
 
         return redirect(url_for('dashboard'))
 
     return render_template('add_article.html', form=form)
 
-# Edit Article
+# Edição de artigo
 @app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
 @is_logged_in
 def edit_article(id):
-    #Faz o select no mongodb de apenas um registro
+    # Faz o consulta no mongoDB pelo ObjectId
     result = collection.find_one({'_id':ObjectId(id)})
 
-    # Get form
+    # Carrega o formulário para a edição do artigo
     form = ArticleForm(request.form)
     
-    #carrega as informações do documento que está sendo alterado
+    # Carrega as informações do documento que está sendo alterado
     form.title.data = result['title']
     form.body.data = result['body']
 
@@ -216,21 +216,21 @@ def edit_article(id):
         #Pega os valores da tela
         title = request.form['title']
         body = request.form['body']
-        #Update mongoDB
+        # Efetua o update no mongoDB
         collection.update_one({'_id':ObjectId(id)}, {'$set': {'title':title,'body':body}})
 
-        flash('Article Updated', 'success')
+        flash('Artigo atualizado com sucesso!', 'success')
 
         return redirect(url_for('dashboard'))
     return render_template('edit_article.html', form=form)
 
-# Delete Article
+# Exclusão de artigo
 @app.route('/delete_article/<string:id>', methods=['POST'])
 @is_logged_in
 def delete_article(id):
-    # Delete de arquivo mongodb
+    # Delete no mongoDB utilizando o ObjectId
     collection.remove({'_id':ObjectId(id)})
-    flash('Article Deleted', 'success')
+    flash('Artigo deletado com sucesso', 'success')
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
